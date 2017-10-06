f8ee55e97328230e658f8106fee4f54d5902b6ba
Avanti Chande <avc8@uw.edu>
2017-10-06 13:25:09 -0700

    Modified setup.sh Broken URL

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 # Change to Data Directory
 cd data
 #download data
-curl --output pronto.csv "https://data.seattle.gov/api/views/tw7j-dfaw/rows.csv?accessType=DOWNLOAD"
+curl --output pronto.csv "fghfhhttps://data.seattle.gov/api/views/tw7j-dfaw/rows.csv?accessType=DOWNLOAD"
